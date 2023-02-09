f4776bba266819fb8fad7ce0b41d25bd9731b9f1
Márcio Alexandre <110576090+marcioacfilho@users.noreply.github.com>
2023-02-08 22:20:37 -0300

    exercicios-php

---
+++ exercicios.php
@@ -0,0 +1,33 @@
+<?php
+/* ## Exercícios
+- Crie um arquivo chamado `exercicio.php`. Nesse arquivo crie os cálculos abaixo. Na resolução dos exercicios você deve mostrar a expressão. 
+Exemplo: 2 + 2 = 4 
+ - 2+2 
+ - 22+57
+ - 35 - 2 
+ - 12 + 2 
+ - 124 * 7.5
+ - 100 / 31 
+ - 14 + 18 - 3 
+ - 124 * 2 / 5 
+ - (7 + 8 + 9 + 3) / 4
+ - (4 + 7) elevado a 2 
+ - (55 / 3) + 19 */
+
+ ?>
+<?php
+ echo "2+2" . "=" . (2+2) . "<br>";
+ echo "22+57" . "=" . (22+57) . "<br>";
+ echo "35-2" . "=" . (35-2) . "<br>";
+ echo "12+2" . "=" . (12+2) . "<br>";
+ echo "124*7.5" . "=" . (124*7.5) . "<br>";
+ echo "100 / 31 " . "=" . (100 / 31) . "<br>";
+ echo "14 + 18 - 3 " . "=" . (14 + 18 - 3) . "<br>";
+ echo "124 * 2 / 5 " . "=" . (100 / 31) . "<br>";
+ echo "7 + 8 + 9 + 3) / 4 " . "=" . (7 + 8 + 9 + 3 ) / 4 . "<br>";
+ echo "4 + 7 " . "=" . (4 + 7) ** 2 . "<br>";
+ echo "55 / 3) + 19" . (55 / 3 ) + 19 . "<br>";
+
+
+
+?>
